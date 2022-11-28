fad9a42e40f60d52c416df75dbb228e103550f77
zhamalieva <88711112+zhamalieva@users.noreply.github.com>
2022-11-28 16:36:54 +0600

    Create 1.py

---
+++ assignment 4/json/1.py
@@ -0,0 +1,19 @@
+import json
+
+f = open("file.json")
+d = json.load(f)
+
+print('Interface Status','='* 90,sep='\n')
+print('DN' , ' '*60 , 'Description' , ' '*6, 'Speed' , ' '*4 , 'MTU') 
+print('-'*61 , ' ' , "-" *15 , " "*2 , "-"*6 , " "*3 , "-"*6)
+cnt = 0
+for i in d['imdata']:
+    cnt += 1
+    out1=i["l1PhysIf"]["attributes"]["dn"]
+    out11=i["l1PhysIf"]["attributes"]["fecMode"]
+    out111=i["l1PhysIf"]["attributes"]["mtu"]
+
+    print(out1,' '*39, out11,' ' * 2,out111)
+    if cnt == 3:
+        exit()
+
